fd6c38cab52b7502c1280f7677d79cab09fa1637
atmin <fr33rider@gmail.com>
2019-02-24 01:33:37 +0200

    router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,20 @@
 import * as React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import { Home } from "./Home";
 import { Table } from "./Table";
 import { SettingsButton } from "./SettingsButton";
 
-export const App = ({ accountId }: { accountId: string }) => {
-  return (
+export const App = ({ accountId }: { accountId: string }) => (
+  <Router>
     <div className="sans-serif w-100 vh-100 pa4">
-      {accountId} hello <Table />
-      <SettingsButton />
+      <nav>
+        <SettingsButton />
+        <a href="#">home</a>
+        <a href="#table">table</a>
+      </nav>
+
+      <Route exact path="/" component={Home} />
+      <Route path="/table" component={Table} />
     </div>
-  );
-};
+  </Router>
+);
