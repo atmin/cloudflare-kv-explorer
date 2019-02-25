c7b7d547c9e2f8548dc6aa467327438d09ab3e0c
atmin <fr33rider@gmail.com>
2019-02-25 04:02:25 +0200

    show keys for a namespace

--- src/components/RouteHome.tsx
+++ src/components/RouteHome.tsx
@@ -20,6 +20,25 @@ const loadNamespaces = async (
   setNamespaces(response.data.result);
 };
 
+const loadKeys = async (
+  selectedNamespace: { id: string; title: string },
+  setKeys: Function,
+  accountId: string,
+  authEmail: string,
+  authKey: string
+) => {
+  const response = await axios({
+    url: `https://api.cloudflare.com/client/v4/accounts/${accountId}/storage/kv/namespaces/${
+      selectedNamespace.id
+    }/keys`,
+    headers: {
+      "X-Auth-Email": authEmail,
+      "X-Auth-Key": authKey
+    }
+  });
+  setKeys(response.data.result);
+};
+
 export const RouteHome = ({
   useLocalStorage
 }: {
@@ -29,27 +48,68 @@ export const RouteHome = ({
   const [authEmail] = useLocalStorage("authEmail");
   const [authKey] = useLocalStorage("authKey");
 
-  const [namespaces, setNamespaces] = useState(false as boolean | Object[]);
+  const [namespaces, setNamespaces] = useState(null);
   useEffect(() => {
     if (!namespaces) {
       loadNamespaces(setNamespaces, accountId, authEmail, authKey);
     }
   });
 
+  const [selectedNamespace, setSelectedNamespace] = useState(null);
+  const [keys, setKeys] = useState(null);
+  useEffect(() => {
+    if (!keys && selectedNamespace) {
+      loadKeys(
+        selectedNamespace as any,
+        setKeys,
+        accountId,
+        authEmail,
+        authKey
+      );
+    }
+  });
+
+  const [selectedKey, setSelectedKey] = useState(null);
+  useEffect(() => {
+    if (selectedKey) {
+      console.log(selectedKey);
+    }
+  });
+
   return (
     <div>
       {accountId && authEmail && authKey ? (
-        <React.Fragment>
-          <h2>Namespaces</h2>
-          {!namespaces && <div>loading...</div>}
-          {namespaces && (
-            <Table
-              header={["Id", "Title"]}
-              data={namespaces}
-              render={row => [row.id, row.title]}
-            />
+        <article className="cf">
+          <div className="fl w-100 w-50-l pr4">
+            <h2>Namespaces</h2>
+            {!namespaces && <div>loading...</div>}
+            {namespaces && (
+              <Table
+                header={["Id", "Title"]}
+                data={namespaces}
+                render={(row: any) => [row.id, row.title]}
+                onRowSelect={(row: any) => {
+                  setSelectedNamespace(row);
+                  setKeys(null);
+                }}
+              />
+            )}
+          </div>
+          {selectedNamespace && (
+            <div className="fl w-100 w-50-l">
+              <h2>Keys</h2>
+              {!keys && <div>loading...</div>}
+              {keys && (
+                <Table
+                  header={["Key"]}
+                  data={keys}
+                  render={(row: any) => [row.name]}
+                  onRowSelect={(row: any) => setSelectedKey(row)}
+                />
+              )}
+            </div>
           )}
-        </React.Fragment>
+        </article>
       ) : (
         <h2>Home</h2>
       )}
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,39 +1,58 @@
 import * as React from "react";
+import { useState } from "react";
 
 export const Table = ({
   header,
   render,
-  data
+  data,
+  onRowSelect
 }: {
   header: string[];
   render: (row: Object) => JSX.Element[];
-  data: Object[];
-}) => (
-  <div className="overflow-auto">
-    <table className="f6 w-100 mw8 center" cellSpacing="0">
-      <thead>
-        <tr>
-          {header.map(th => (
-            <th
-              key={JSON.stringify(th)}
-              className="fw6 bb b--black-20 tl pb3 pr3 bg-white"
-            >
-              {th}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody className="lh-copy">
-        {data.map(datum => (
-          <tr key={JSON.stringify(datum)}>
-            {render(datum).map(td => (
-              <td key={JSON.stringify(td)} className="pv3 pr3 bb b--black-20">
-                {td}
-              </td>
+  data?: Object[];
+  onRowSelect?: (row: Object) => void;
+}) => {
+  const [selected, setSelected] = useState({});
+  return (
+    <div className="overflow-auto">
+      <table className="f6 w-100 mw8 center" cellSpacing="0">
+        <thead>
+          <tr>
+            {header.map(th => (
+              <th
+                key={JSON.stringify(th)}
+                className="fw6 bb b--black-20 tl pb3 ph3 bg-white"
+              >
+                {th}
+              </th>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
-);
+        </thead>
+        <tbody className="lh-copy">
+          {data &&
+            data.map(row => (
+              <tr
+                key={JSON.stringify(row)}
+                className={selected === row ? "bg-black-80 white" : ""}
+                onClick={() => {
+                  if (onRowSelect) {
+                    setSelected(row);
+                    onRowSelect(row);
+                  }
+                }}
+              >
+                {render(row).map(td => (
+                  <td
+                    key={JSON.stringify(td)}
+                    className="pv3 ph3 bb b--black-20"
+                  >
+                    {td}
+                  </td>
+                ))}
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
